8ec9e7c817bb36a0f77e80c82e54a896b4d787b7
Eric Dahan <Ericdahan@dyn-160-39-244-190.dyn.columbia.edu>
2019-02-07 19:25:32 -0500

    improved JS and layout

---
+++ Assets/Javascript/game.js
@@ -0,0 +1,21 @@
+//Logic for the Javascript
+
+//Array for characters
+
+//one containers for characters
+
+//one container for enemies selector
+
+//one for defender area and attack
+
+//show them in all each area to make them hide
+
+//Logic
+
+//javascript for clicking characters to hide
+
+$(document).ready(function() {
+  $("container").click(function() {
+    $("#healthpoints").hide();
+  });
+});
