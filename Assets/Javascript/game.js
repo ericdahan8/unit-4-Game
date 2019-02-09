a633b162087b0edb3cd5b7908591cfa32150b3a0
Eric Dahan <Ericdahan@dyn-160-39-244-41.dyn.columbia.edu>
2019-02-09 15:42:41 -0500

    Attack sequence working

--- Assets/Javascript/game.js
+++ Assets/Javascript/game.js
@@ -14,8 +14,31 @@
 
 //javascript for clicking characters to hide
 
+//when you click obi won show other guys then hide on click
+
 $(document).ready(function() {
-  $("container").click(function() {
-    $("#healthpoints").hide();
+  $(".G1").on("click", function() {
+    var obi1div = $(".G1").detach();
+    $(".D1").append(obi1div);
+  });
+
+  $(".G2").on("click", function() {
+    var obi1div = $(".G2").detach();
+    $(".D1").append(obi1div);
+  });
+
+  $(".G3").on("click", function() {
+    var obi1div = $(".G3").detach();
+    $(".D1").append(obi1div);
+  });
+
+  $(".G4").on("click", function() {
+    var obi1div = $(".G4").detach();
+    $(".D1").append(obi1div);
+  });
+
+  $(obi1div).on("click", function() {
+    var newDiv = $(obi1div).detach();
+    $(".E1").appendTo(newDiv);
   });
 });
